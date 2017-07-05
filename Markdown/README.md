a2bd2ff96011526e96b34b6292accda1638d49c7
kouchengjian <hui793114465@163.com>
2017-07-05 11:13:15 +0800

    init

---
+++ Markdown/README.md
@@ -0,0 +1,90 @@
+# MarkDown
+
+> Android平台的原生Markdown解析器，已整合进 [RichText](https://github.com/zzhoujay/RichText)
+
+* 由markdown文本直接转换为Spanned，快捷高效
+* 不依赖特定控件，低侵入性
+* 遵循 Github Flavored Markdown 标准
+
+### 效果展示
+
+![效果图](image/img1.jpg)
+
+### 使用
+
+```
+Markdown.fromMarkdown(text,imageGetter,textView);
+```
+**注意：** 此方法需要在textView的Measure完成后调用，因为需要获取textView的宽高
+
+例子：
+```
+textView.post(new Runnable() {
+     @Override
+     public void run() {
+     Spanned spanned = MarkDown.fromMarkdown(stream, new Html.ImageGetter() {
+           @Override
+           public Drawable getDrawable(String source) {
+                 Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
+                 drawable.setBounds(0, 0, 400, 400);
+                 return drawable;
+           }
+     }, textView);
+     textView.setText(spanned);
+}
+```
+
+### 在RichText中使用
+
+[RichText](https://github.com/zzhoujay/RichText) 包含了一些对图片和其它东西的处理，使用更简单
+```
+RichText.fromMarkdown(markdown).into(textView);
+```
+
+### Use in Gradle
+
+`compile 'com.zzhoujay.markdown:markdown:1.0.5'`
+
+
+### 已知问题
+
+* 暂不支持使用反斜杠 \\ 转义
+* 不支持表格
+* TextView设置了`setTextIsSelectable(true)`后和长按事件冲突
+* 暂时不支持内嵌html
+
+
+### 后续计划
+
+* 修复一些已知问题
+* 加入多选框支持
+* 增加对简单html标签的支持
+
+### License
+
+```
+The MIT License (MIT)
+
+Copyright (c) 2015 zzhoujay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+_by zzhoujay_
---
+++ Markdown/app/src/main/java/com/zzhoujay/markdowndemo/MainActivity.java
@@ -0,0 +1,66 @@
+package com.zzhoujay.markdowndemo;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.zzhoujay.markdown.MarkDown;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String test_str = "# MarkDown\n" +
+            "\n" +
+            "> Android平台的Markdown解析器\n" +
+            "\n" +
+            "##### 开发中。。。\n" +
+            "\n" +
+            "__by zzhoujay__\n";
+
+    private static final String markdown_test = "image: ![image](http://image.tianjimedia.com/uploadImages/2015/129/56/J63MI042Z4P8.jpg)\n[link](https://github.com/zzhoujay/RichText/issues)";
+
+    private static final String empty_string = "  \n   \n  \n# hello";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        assert textView != null;
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        final InputStream stream = getResources().openRawResource(R.raw.hello);
+
+        textView.post(new Runnable() {
+            @Override
+            public void run() {
+                long time = System.nanoTime();
+                Spanned spanned = MarkDown.fromMarkdown(markdown_test, new Html.ImageGetter() {
+                    public static final String TAG = "Markdown";
+
+                    @Override
+                    public Drawable getDrawable(String source) {
+                        Log.e(TAG, "getDrawable() called with: source = [" + source + "]");
+                        Drawable drawable = new ColorDrawable(Color.LTGRAY);
+                        drawable.setBounds(0, 0, textView.getWidth() - textView.getPaddingLeft() - textView.getPaddingRight(), 400);
+                        return drawable;
+                    }
+                }, textView);
+                long useTime = System.nanoTime() - time;
+                Toast.makeText(getApplicationContext(), "use time:" + useTime, Toast.LENGTH_LONG).show();
+                textView.setText(spanned);
+            }
+        });
+
+    }
+}
